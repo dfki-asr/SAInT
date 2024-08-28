6f6f3a694828ee34461cbaf3e6087f7bed3726f7
Manuela Schuler <Manuela.Schuler@dfki.de>
2024-08-28 17:35:22 +0200

    run_dash_application: - rename main() to run_dash_application - __init__.py includes the function as "run" usage:     import SAInT     SAInT.run()

--- src/SAInT/__init__.py
+++ src/SAInT/__init__.py
@@ -1,34 +1,6 @@
-from SAInT.networks import SVModel, MultiOutputRidgeModel, \
-    MultiOutputRandomForestModel, RandomForestModel, \
-    MLP, TabResNet, FastAIModel, XGBModel, GammmaRegressorModel, \
-    do_grid_search
-from SAInT.sa import GlobalExplainer, LocalLimeExplainer, \
-    LocalShapExplainer
-from SAInT.common import set_seed, makedirs, exists, load_json_dict
-from SAInT.normalization import NormalizationMeanStdValues, MeanStdNormalizer, NormalizationMinMaxValues, MinMaxNormalizer
-from SAInT.data_augmentation import OnlineDataAugmentation
-from SAInT.online_normalization import OnlineMeanStdNormalization, OnlineMinMaxNormalization
-from SAInT.dataloader import DataLoader, create_dataloader
-from SAInT.data_settings import DataSettings
-from SAInT.dataset import Dataset
-from SAInT.metric import get_metric
-from SAInT.model import Model
-from SAInT.trainer import Trainer
-from SAInT.data_visualizer import DataVisualizer
-from SAInT.dash_application.saint_dash_app import MySAInTDashApplication
+from SAInT.run_dash_application import run_dash_application as run
 
 
 __all__ = [
-    "set_seed", "makedirs", "exists", "Model", "GlobalExplainer",
-    "LocalLimeExplainer", "LocalShapExplainer",
-    "FastAIModel", "MLP", "TabResNet", "SVModel",
-    "MultiOutputRidgeModel", "XGBModel", "GammmaRegressorModel",
-    "do_grid_search", "MultiOutputRandomForestModel", "RandomForestModel",
-    "DataLoader", "DataSettings", "create_dataloader", "Dataset",
-    "plot_histogram", "plot_correlation_matrix", "get_metric",
-    "NormalizationMeanStdValues", "NormalizationMinMaxValues",
-    "MeanStdNormalizer", "MinMaxNormalizer",
-    "OnlineDataAugmentation", "OnlineMeanStdNormalization",
-    "OnlineMinMaxNormalization", "DataVisualizer",
-    "Trainer", "load_json_dict", "MySAInTDashApplication"
+    "run"
 ]
--- src/SAInT/run_dash_application.py
+++ src/SAInT/run_dash_application.py
@@ -1,9 +1,9 @@
 from SAInT.dash_application.saint_dash_app import MySAInTDashApplication
 
-def main():
+def run_dash_application():
     application = MySAInTDashApplication()
     application.run()
 
 
 if __name__ == "__main__":
-    main()
+    run_dash_application()
