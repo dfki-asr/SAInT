ad7d71d6539dd508009bf451195f42a68e1a19b1
Manuela Schuler <Manuela.Schuler@dfki.de>
2024-09-04 16:55:07 +0200

    BUGFIX relative values    lime_height, shap_height,    error_plot_height, error_plot_width    now dependend on screen resolution

--- src/SAInT/dash_application/pixel_definitions.py
+++ src/SAInT/dash_application/pixel_definitions.py
@@ -8,11 +8,7 @@ padding = "7px"
 
 # relative
 logo_height = "6.5%"
-lime_height = "65%"
-shap_height = "35%"
-error_plot_height = "10%"
-error_plot_width = "30%"
-lime_expl_width = "33%"
+lime_expl_width = "65%"
 tab_gap = "1.443%"
 
 width = 1386
@@ -23,6 +19,10 @@ feature_popup_width = f"{int(width / 100 * 30)}px"
 area_height = f"{int(height / 100 * 25)}px"
 editor_window_height = f"{int(height / 100 * 70)}px"
 figure_width = int(width / 100 * 93)
-default_figure_height = int(height / 100 * 66)
+default_figure_height = int(height / 100 * 62)
 marker_size = int(width / 100 * 0.8)
+lime_height = f"{int(height / 100 * 32)}px"
+shap_height = f"{int(height / 100 * 22)}px"
+error_plot_height = f"{int(height / 100 * 15)}px"
+error_plot_width = f"{int(width / 100 * 50)}px"
 lsa_figure_width = figure_width
